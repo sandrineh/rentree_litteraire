900495cd3cb5059e5bda6734f7ed7ba0aa2ea195
Sandrine <eleonore.henry@gmail.com>
2024-01-23 20:31:18 +0100

    retrait librairie plotly du fichier principal

--- RL_2023.py
+++ RL_2023.py
@@ -21,9 +21,9 @@ import time
 
 #Datavisualisation
 #from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import plotly.express as px
-import matplotlib.pyplot as plt
+#import plotly.graph_objects as go
+#import plotly.express as px
+#import matplotlib.pyplot as plt
 
 import streamlit as st
 from streamlit_option_menu import option_menu
